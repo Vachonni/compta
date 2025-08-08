e9a03629e29a787d94fef604c2a1294620225a14
Nicholas Vachon MBANV <nicholasvachon@hotmail.com>
2025-08-08 16:02:32 -0400

    simplified and universalised databases_dir

--- services/database/src/database_pkg/config/settings.py
+++ services/database/src/database_pkg/config/settings.py
@@ -1,32 +1,19 @@
 from pathlib import Path
 from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
-import os
 
 from database_pkg.config.schemas import AppEnvEnum
-
-env_file: str = f".env.{os.getenv('APP_ENV', 'local')}"
 
 
 class DatabaseSettings(BaseSettings):
     """Settings for the database service."""
 
     SERVICE_NAME: str = "database"
-    DATABASES_DIR_LOCAL: str = "/Users/nicholasvachon/Databases"
-    DATABASES_DIR_DOCKER: str = "/app/DatabasesMount"
+    DOCKER_DATABASES_DIR: str
+    LOCAL_DATABASES_DIR: str
     app_env: AppEnvEnum = Field(default=AppEnvEnum.LOCAL)
 
-    model_config = SettingsConfigDict(
-        env_file=env_file,
-        env_file_encoding="utf-8",
-    )
-
-    @property
-    def databases_dir(self) -> str:
-        if self.app_env == AppEnvEnum.LOCAL:
-            return self.DATABASES_DIR_LOCAL
-        else:
-            return self.DATABASES_DIR_DOCKER
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
     @property
     def log_level(self) -> str:
@@ -37,7 +24,10 @@ class DatabaseSettings(BaseSettings):
 
     @property
     def db_path(self) -> Path:
-        return Path(self.databases_dir) / "compta"
+        if self.app_env == AppEnvEnum.LOCAL:
+            return Path(self.LOCAL_DATABASES_DIR)
+        else:
+            return Path(self.DOCKER_DATABASES_DIR)
 
     @property
     def blob_path(self) -> Path:
@@ -64,7 +54,6 @@ database_settings = DatabaseSettings()  # type: ignore
 if __name__ == "__main__":
     print("Configuration paths:")
     print(f"Environment: {database_settings.app_env.value}")
-    print(f"Databases directory: {database_settings.databases_dir}")
     print(f"Database path: {database_settings.db_path}")
     print(f"Blob path: {database_settings.blob_path}")
     print(f"SQLite development path: {database_settings.sqlite_path}")
