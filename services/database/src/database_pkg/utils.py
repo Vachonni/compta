ef935cbc20cd7c046dedfe0adfb40db98500510a
Nicholas Vachon MBANV <nicholasvachon@hotmail.com>
2025-08-09 16:51:07 -0400

    logs

--- services/database/src/database_pkg/utils.py
+++ services/database/src/database_pkg/utils.py
@@ -10,7 +10,9 @@ logger = logging.getLogger(__name__)
 
 def get_db_connection():
     db_path = str(database_settings.sqlite_path)
-    logger.debug(f"Connecting to database at: {db_path}")
+    logger.debug(
+        f"Connecting to database at: {db_path} with app_env: {database_settings.app_env}"
+    )
     conn = sqlite3.connect(db_path, check_same_thread=False)
     conn.row_factory = sqlite3.Row
     return conn
